39ea9332a96b060b8255341006584056a6c348ee
Kevin Henneigh <55999394+kevinhenneigh@users.noreply.github.com>
2020-10-09 19:09:17 -0700

    Set up Identity framework (#9)

---
+++ JamAmp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JamAmp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JamAmp.Areas.Identity.IdentityHostingStartup))]
+namespace JamAmp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<JamAmpDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("JamAmpDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<JamAmpDbContext>();
+            });
+        }
+    }
+}
