e2add64beaaeb9bfde95ffc22da77484a51dc7f6
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-10-12 09:34:09 -0700

    Changed required account and modified password requirements

--- JamAmp/Startup.cs
+++ JamAmp/Startup.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using JamAmp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using JamAmp.Models;
 
 namespace JamAmp
 {
@@ -23,8 +28,14 @@ namespace JamAmp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<JamAmpDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions);
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
+
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
