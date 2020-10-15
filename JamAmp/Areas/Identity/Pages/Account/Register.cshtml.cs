da45f224a32c23c9ebdbe14cbc33f8cad2273dac
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-10-14 20:05:23 -0700

    Changed Login from Email to UserName Added UserName to Registration page

--- JamAmp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JamAmp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -56,6 +56,9 @@ namespace JamAmp.Areas.Identity.Pages.Account
             [Display(Name = "Password")]
             public string Password { get; set; }
 
+            [Required]
+            public string UserName { get; set; }
+
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -74,7 +77,7 @@ namespace JamAmp.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
